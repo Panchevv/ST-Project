9f9f8ff83c6b2175006e67a9e8beacd72434cdc5
niksata998 <nkrivulev7@gmail.com>
2022-12-01 13:08:18 +0200

    TicketService and TicketServiceImpl update

--- project/src/main/java/com/todo/project/service/impl/TicketServiceImpl.java
+++ project/src/main/java/com/todo/project/service/impl/TicketServiceImpl.java
@@ -1,28 +1,38 @@
 package com.todo.project.service.impl;
 
+import com.todo.project.exceptions.EntityNotFoundException;
 import com.todo.project.persistence.model.Ticket;
+import com.todo.project.persistence.model.User;
+import com.todo.project.persistence.repository.TicketRepository;
 import com.todo.project.service.TicketService;
 import java.util.List;
 
 public class TicketServiceImpl implements TicketService {
 
+    private final TicketRepository ticketRepository;
+
+    public TicketServiceImpl(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
     @Override
-    public Ticket createTicket() {
+    public void createTicket(Ticket ticket, User createdBy) {
+
+        ticketRepository.create(ticket);
+    }
+
+    @Override
+    public void updateTicket(Ticket ticket) {
+        ticketRepository.update(ticket);
+    }
+
+    @Override
+    public List<Ticket> findTickets(Integer userId) { //?
         return null;
     }
 
     @Override
-    public Ticket updateTicket() {
-        return null;
-    }
-
-    @Override
-    public List<Ticket> findTickets(Integer userId) {
-        return null;
-    }
-
-    @Override
-    public Ticket saveTicker(Ticket ticket) {
+    public Ticket saveTicker(Ticket ticket) { //?
         return null;
     }
 }
