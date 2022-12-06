5ed3121a9afdb06c71ebade645d0eb8c96ea8dfa
nKrivulev <118992683+nKrivulev@users.noreply.github.com>
2022-12-06 14:20:34 +0200

    Tracks (#1)
    * EntityNotFoundException
    
    * UserRepository
    
    * UserRepositoryImpl
    
    * TicketRepositoryImpl
    
    * Ticket model
    
    * TicketService and TicketServiceImpl update
    
    * User model
    
    * TicketService update
    
    * Added Data annotation in Ticket and User models

--- project/src/main/java/com/todo/project/persistence/model/Ticket.java
+++ project/src/main/java/com/todo/project/persistence/model/Ticket.java
@@ -1,24 +1,99 @@
 package com.todo.project.persistence.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 
+import java.util.Date;
+import java.util.Objects;
+
 @Data
 @Entity
-@Table(name = "TICKET")
+@Table(name = "tickets")
 public class Ticket {
-    // TODO:
-    // Crate ticket domain
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
-    @Column(name = "id", nullable = false, updatable = false)
+    @Column(name = "id")
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name="creator")
+    private User creator;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "dueDate")
+    private Date dueDate;
+
+    public Ticket() {
+    }
+
+    public Ticket(Long id, User creator, String description, String title, Date dueDate) {
+        this.id = id;
+        this.creator = creator;
+        this.description = description;
+        this.title = title;
+        this.dueDate = dueDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return id == ticket.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- project/src/main/java/com/todo/project/persistence/model/User.java
+++ project/src/main/java/com/todo/project/persistence/model/User.java
@@ -14,13 +14,70 @@ import lombok.Setter;
 @Entity
 @Table(name = "Users")
 public class User {
-    // TODO: add user fields
-    // TODO: add all necessary other domains as classes in the domain package
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
-    @Column(name = "id", nullable = false, updatable = false)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User user = (User) obj;
+
+        return id == user.id;
+    }
 }
